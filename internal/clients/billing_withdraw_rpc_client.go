567f9d8e5c676f4f06ceeb0f8d40b06cc3d8fa66
Лапшин Иван Александрович <ilapshin@phoenixit.ru>
2025-03-06 21:31:42 +0300

    вынес миграцию в отдельный процесс

--- internal/clients/billing_withdraw_rpc_client.go
+++ internal/clients/billing_withdraw_rpc_client.go
@@ -7,7 +7,10 @@ import (
 	"github.com/vysogota0399/gophermart_portal/internal/api/logging"
 	"github.com/vysogota0399/gophermart_portal/internal/config"
 	billing "github.com/vysogota0399/gophermart_protos/gen/commands/withdraw"
+	"github.com/vysogota0399/gophermart_protos/gen/entities"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"google.golang.org/genproto/googleapis/type/money"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -37,17 +40,17 @@ func NewBillingWithdrawRpcClient(lc fx.Lifecycle, cfg *config.Config, lg *loggin
 	}, nil
 }
 
-func (rpc *BillingWithdrawRpcClient) Withdraw(ctx context.Context, accountID int64, amount float64, orderNumber string) error {
-	params := &billing.WithdrawParams{
-		AccountId:   accountID,
-		Amount:      amount,
+func (rpc *BillingWithdrawRpcClient) DoWithdraw(ctx context.Context, accountID int64, amount float64, orderNumber string) error {
+	params := &billing.DoWithdrawParams{
+		Account:     &entities.Account{Id: accountID},
+		Amount:      &money.Money{Units: int64(amount * 100)},
 		OrderNumber: orderNumber,
 	}
 
 	rpc.lg.DebugCtx(
 		ctx,
 		"do withdraw",
-		params,
+		zap.Any("params", params),
 	)
 	_, err := rpc.billing.DoWithdraw(ctx, params)
 	if err != nil {
